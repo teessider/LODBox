3ae52f5f43e22c85c76d98874b0b576c7304c861
teessider <andrew@andybellart.co.uk>
2019-01-14 02:28:53 +0100

    Merging scenes together works! :D Importing and merging scenes can be done a loopable fashion. Turns out I was always exporting the original scene in my export_fbx() function :| - Added docstrings to export_fbx() function and changed some constant values to arguments - Added reference to ANOTHER test file - changed some whitespace

--- lodbox/testy_mctestface.py
+++ lodbox/testy_mctestface.py
@@ -8,21 +8,40 @@ file_paths = {'Attributes': "Sphere_Attr.fbx",
               'Group_lods': "Sphere_group_lods.fbx",
               'MergeSceneTest01': "MergeSceneTest01.FBX",
               'MergeSceneTest02': "MergeSceneTest02.FBX",
-              'MergeSceneTest03': "MergeSceneTest03.FBX"}  # Hardcoded for now
+              'MergeSceneTest03': "MergeSceneTest03.FBX",
+              'MergeSceneTest_Merged': "MergeSceneTest_Merged.FBX",
+              'test_merged_scenes': "test_merged_scenes.fbx"}  # Hardcoded for now
 
 
-def export_fbx(fbx_manager, fbx_scene, filename):
+def export_fbx(fbx_manager, fbx_scene, version, filename):
+    """
+    Exports scene as FBX file.\n
+    See "FBX SDK C++ API Reference > Files > File List > fbxsdk > fileio > fbx" for list of valid inputs for version.
+
+    :param fbx_manager: FBX Manager
+    :type fbx_manager: fbx.FbxManager
+    :param fbx_scene: FBX Scene
+    :type fbx_scene: fbx.FbxScene
+    :param version: FBX File Version
+    :type version: string
+    :param filename: Export file name
+    :type filename: string
+    :return:
+    :rtype: None
+    """
     # Export the file. Same process as importing - Create the Exporter, Initialize it, export!
-    exporter = fbx.FbxExporter.Create(fbx_manager, 'Exporter')
+    exporter = fbx.FbxExporter.Create(fbx_manager, 'Exporter')  # type: fbx.FbxExporter
 
     # The FbxSceneRenamer() is only used for FBX version, makes sure the string doesn't contain characters the various formats/programs don't like
     scene_renamer = fbx.FbxSceneRenamer(fbx_scene)
 
     # Most of the IO settings are True by default so no need to set any...for now!
     # I got this string from fbxio.h (which works in 2015) in FBX SDK Reference > Files > File List > fbxsdk > fileio > fbx
-    exporter.SetFileExportVersion('FBX201400', scene_renamer.eFBX_TO_FBX)
-    exporter.Initialize('{}.fbx'.format(filename), -1, manager.GetIOSettings())
-    exporter.Export(scene)
+    exporter.SetFileExportVersion(version, scene_renamer.eFBX_TO_FBX)
+    # ASCII=2 for pFileformat and -1/0/1 is Binary
+    # Look into FbxIOPluginRegistry for more info
+    exporter.Initialize(filename, -1, manager.GetIOSettings())
+    exporter.Export(fbx_scene)  # MAKE SURE THIS MATCHES THE INPUT SCENE >(
     exporter.Destroy()  # Make sure to destroy the scene exporter afterwards!
 
 
@@ -34,6 +53,7 @@ global_settings = scene.GetGlobalSettings()  # type: fbx.FbxGlobalSettings
 # FbxCommon.LoadScene(manager, scene, file_paths['lodGroup'])
 # FbxCommon.LoadScene(manager, scene, file_paths['Group_lods'])
 FbxCommon.LoadScene(manager, scene, file_paths['MergeSceneTest01'])
+# FbxCommon.LoadScene(manager, scene, file_paths['MergeSceneTest_Merged'])
 
 root_node = scene.GetRootNode()  # type: fbx.FbxNode
 
@@ -107,7 +127,7 @@ for node in scene_nodes:
 
         node.SetNodeAttribute(lod_group_attr)  # This is VIP!!! Don't forget about this again! xD
 
-        export_fbx(manager, scene, "test_lod_group")
+        export_fbx(manager, scene, "201400", "test_lod_group")
 
     # # FbxLODGroup > FbxNull # #
     # "Extracting" normal meshes out of LOD Groups so don't have to deal with that in 3ds Max/Maya (at least 1st steps)
@@ -171,7 +191,9 @@ for node in scene_nodes:
                         custom_prop.Destroy()
 
                     elif custom_prop.HasMinLimit() and custom_prop.HasMaxLimit():
-                        print("{}\n  type: {}\n\tValue: {}\n\tMinLimit: {}\n\tMaxLimit: {}".format(custom_prop.GetName(), data.GetName(), custom_prop.Get(), custom_prop.GetMinLimit(), custom_prop.GetMaxLimit()))
+                        print("{}\n  type: {}\n\tValue: {}\n\tMinLimit: {}\n\tMaxLimit: {}".format(custom_prop.GetName(), data.GetName(),
+                                                                                                   custom_prop.Get(), custom_prop.GetMinLimit(),
+                                                                                                   custom_prop.GetMaxLimit()))
                     else:
                         print("{}\n  type: {}\n\tValue: {}".format(custom_prop.GetName(), data.GetName(), custom_prop.Get()))
 
@@ -182,7 +204,9 @@ for node in scene_nodes:
                 elif data.GetType() == fbx.eFbxDouble:  # Number type - Similar to float but instead of 32-bit data type, 64-bit data type.
                     custom_prop = fbx.FbxPropertyFloat1(custom_prop)
                     if custom_prop.HasMinLimit() and custom_prop.HasMaxLimit():
-                        print("{}\n  type: {}\n\tValue: {}\n\tMinLimit: {}\n\tMaxLimit: {}".format(custom_prop.GetName(), data.GetName(), custom_prop.Get(), custom_prop.GetMinLimit(), custom_prop.GetMaxLimit()))
+                        print("{}\n  type: {}\n\tValue: {}\n\tMinLimit: {}\n\tMaxLimit: {}".format(custom_prop.GetName(), data.GetName(),
+                                                                                                   custom_prop.Get(), custom_prop.GetMinLimit(),
+                                                                                                   custom_prop.GetMaxLimit()))
                     else:
                         print("\tValue: {}".format(custom_prop.Get()))
 
@@ -201,17 +225,23 @@ for node in scene_nodes:
 
         root_node.AddChild(new_group)  # Make sure it's in the scene!
 
-        export_fbx(manager, scene, "test_no_lod_group")
+        export_fbx(manager, scene, "201400", "test_no_lod_group")
+        manager.Destroy()
 
     # # Merging Scenes Test # #
     # Starting with MergeTestScene01
     elif node.GetName() == "Sphere001":
 
-        # Create a new scene to hold the already imported scene
+        # Create a new scene to hold the already imported scene (probably can just the original normally but this is useful for testing ;) )
         reference_scene = fbx.FbxScene.Create(manager, "ReferenceScene")
-
         # Start moving stuff to new scene (already have the scene nodes in list from above)
         ref_scene_root = reference_scene.GetRootNode()  # type: fbx.FbxNode
+
+        # Since the default Axis System is Y-Up and because these are brand new settings (its made with a scene along with FbxAnimEvaluator and a Root Node),
+        # the axis needs to be set to the same as the original imported scene!
+        orig_axis_sys = fbx.FbxAxisSystem(global_settings.GetAxisSystem())
+        orig_axis_sys.ConvertScene(reference_scene)
+
         # Because this is a test, the original scene_nodes list is used, otherwise this would be the
         # MergeTestScene01 nodes.
         for x in range(0, len(scene_nodes)):
@@ -221,36 +251,73 @@ for node in scene_nodes:
         # so the connections need to be removed. And because there could be lots of children, its better to disconnect the root node from the children.
         root_node.DisconnectAllSrcObject()
 
+        # Because the scene Object also has connections to other types of FBX objects, they need to be moved too.
+        # (I'm guessing) Also since there is only a single mesh in the FBX, the scene has connections to that too.
+        for x in range(0, scene.GetSrcObjectCount()):
+            fbx_obj = scene.GetSrcObject(x)  # type: fbx.FbxObject
+            # Don't want to move the root node, the global settings or the Animation Evaluator (at this point)
+            if fbx_obj == root_node or fbx_obj.GetClassId() == fbx.FbxGlobalSettings.ClassId or type(fbx_obj) == fbx.FbxAnimEvaluator or fbx_obj.ClassId == fbx.FbxAnimStack.ClassId or fbx_obj.ClassId == fbx.FbxAnimLayer.ClassId:
+                continue
+            else:
+                fbx_obj.ConnectDstObject(reference_scene)
+
+        # Now the scene can be disconnected as everything has been moved!
+        scene.DisconnectAllSrcObject()
+
         print("merged stuff starts from here")
         # Now that the first scene has been moved from the original and disconnected, time to start
         # merging MergeTestScene02.
         # It seems a new scene HAS to be created for each scene (perhaps revisit this at some point?)
         # So start off with creating/loading scene to merge in
-        new_scene = fbx.FbxScene.Create(manager, "MergeSceneTest02")
-        FbxCommon.LoadScene(manager, new_scene, file_paths['MergeSceneTest02'])
-        new_root_node = new_scene.GetRootNode()
-        # Just using the scene_nodes list again - careful with this though as it could lead to human errors?
-        scene_nodes = [new_root_node.GetChild(i) for i in range(0, new_root_node.GetChildCount())]
+        # EDIT: NOPE - I WAS JUST ALWAYS EXPORTING THE ORIGINAL SCENE - The original scene can be used! :D
+        FbxCommon.LoadScene(manager, scene, file_paths['MergeSceneTest02'])
+        scene_nodes = [root_node.GetChild(i) for i in range(0, root_node.GetChildCount())]
 
         # Repeat adding the new scene nodes to the reference scene and disconnecting to old one
         for x in range(0, len(scene_nodes)):
             child = scene_nodes[x]
             ref_scene_root.AddChild(child)
-        new_root_node.DisconnectAllSrcObject()
+        root_node.DisconnectAllSrcObject()
 
-        print("2nd merged stuff starts from here")
-        # Now starting to merge MergeSceneTest03. Not quite sure but I can reuse the last scene variables
-        # (Couldn't do that with the 2nd/1st scene - perhaps because of testing environment/Garbage Collection?)
-        new_scene = fbx.FbxScene.Create(manager, "MergeSceneTest03")
-        FbxCommon.LoadScene(manager, new_scene, file_paths['MergeSceneTest03'])
-        new_root_node = new_scene.GetRootNode()
-        scene_nodes = [new_root_node.GetChild(i) for i in range(0, new_root_node.GetChildCount())]
+        # # Move other types of scene objects again
+        for x in range(0, scene.GetSrcObjectCount()):
+            fbx_obj = scene.GetSrcObject(x)  # type: fbx.FbxObject
+            # Don't want to move the root node, the global settings or the Animation Evaluator (at this point)
+            if fbx_obj == root_node or fbx_obj.GetClassId() == fbx.FbxGlobalSettings.ClassId or type(fbx_obj) == fbx.FbxAnimEvaluator or fbx_obj.ClassId == fbx.FbxAnimStack.ClassId or fbx_obj.ClassId == fbx.FbxAnimLayer.ClassId:
+                continue
+            else:
+                fbx_obj.ConnectDstObject(reference_scene)
+        scene.DisconnectAllSrcObject()  # DON'T FORGET TO DISCONNECT THE ORIGINAL SCENE FROM THE MOVED OBJECTS!
+
+        # ## 2nd MERGE STUFF
+        FbxCommon.LoadScene(manager, scene, file_paths['MergeSceneTest03'])
+        scene_nodes = [root_node.GetChild(i) for i in range(0, root_node.GetChildCount())]
         for x in range(0, len(scene_nodes)):
             child = scene_nodes[x]
             ref_scene_root.AddChild(child)
+        root_node.DisconnectAllSrcObject()
+
+        # Move other types of scene objects again
+        for x in range(0, scene.GetSrcObjectCount()):
+            fbx_obj = scene.GetSrcObject(x)  # type: fbx.FbxObject
+            # Don't want to move the root node, the global settings or the Animation Evaluator (at this point)
+            if fbx_obj == root_node or fbx_obj.GetClassId() == fbx.FbxGlobalSettings.ClassId or type(fbx_obj) == fbx.FbxAnimEvaluator or fbx_obj.ClassId == fbx.FbxAnimStack.ClassId or fbx_obj.ClassId == fbx.FbxAnimLayer.ClassId:
+                continue
+            else:
+                print(fbx_obj.GetClassId().GetName())
+                fbx_obj.ConnectDstObject(reference_scene)
+        scene.DisconnectAllSrcObject()  # DON'T FORGET TO DISCONNECT THE ORIGINAL SCENE FROM THE MOVED OBJECTS!
 
         # ## FBX EXPORT ##
-        export_fbx(manager, scene, "test_merged_scenes")
+        # Okay so it works! BUT it seems to be almost double the file size than if I would have exported them from 3ds Max (or Maya)?!
+        # EDIT: I found the cause :D when comparing the files as ASCII, the FBX version has Tangents and Binormals so that is the extra data :)
+        # Normally, I don't export these so hence my confusion! I wonder if they can be excluded....?
+        export_fbx(manager, reference_scene, "201400", "test_merged_scenes")
+
+        for x in range(0, reference_scene.GetSrcObjectCount()):
+            print(reference_scene.GetSrcObject(x), reference_scene.GetSrcObject(x).GetName())
+
+        manager.Destroy()
 
     else:
         print(node.GetName(), type(node))
